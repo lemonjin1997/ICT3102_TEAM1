3b2d9dbff05024a94a5a4ce3926e6e8ad18ce15c
lemonjin1997 <53006700+lemonjin1997@users.noreply.github.com>
2021-09-08 04:14:35 +0800

    Flask Server with DB

---
+++ Performance/Test.py
@@ -0,0 +1,13 @@
+from fastapi import FastAPI
+from typing import Optional
+app = FastAPI()
+
+
+@app.get("/")
+async def read_root():
+    return {"Hello": "World"}
+
+
+@app.get("/items/{item_id}")
+async def read_item(item_id: int, q: Optional[str] = None):
+    return {"item_id": item_id, "q": q}
