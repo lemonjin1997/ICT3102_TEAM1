97ad25e40a0482c3f1da2cc29ae6048d3720c05a
lemonjin1997 <53006700+lemonjin1997@users.noreply.github.com>
2021-11-26 19:42:23 +0800

    push update to test.py

--- mainapp/app/src/test.py
+++ mainapp/app/src/test.py
@@ -1,13 +1,19 @@
 from timeit import timeit
+import statistics
 
 total_time = 0
-
+test_data = []
 for x in range(50):
     tmp_time = 0
     tmp_time = timeit("insert_ping(staff_id, beacon_mac, RSSI)", 'from crud.crud import insert_ping;  staff_id = "123"; beacon_mac = "E1C328D7D152"; RSSI = "-51"; ', number=1)
     total_time += tmp_time
     print("time take for ", x+1 ," run :" ,tmp_time)
+    test_data.append(tmp_time)
 average_time = total_time/50
 
+print("min: ", min(test_data))
+print("max: ", max(test_data))
+print("mode: " , statistics.mode(test_data))
+print("median: " , statistics.median(test_data))
 print("total time: ", total_time)
 print("average time: ", average_time)
